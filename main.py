6a56edb5d861b300e3bd356b794a2a1222bf83fa
NASTJA <anastasiaizotova8010@gmail.com>
2024-04-24 21:41:49 +0300

    dz2com

--- main.py
+++ main.py
@@ -13,6 +13,45 @@ class Category:
         Category.total_category += 1
         Category.total_unique += len(set(self.products))
 
+    def __len__(self):
+        count_products = 0
+        for product in self.products:
+            count_products += product.quantity
+        return count_products
+
+    def __str__(self):
+        return f'{self.title}, количество продуктов: {len(self)} шт.'
+
+    def adding_product(self, new_product):
+        if new_product.quentity == 0:
+            raise ValueError('Нельзя складывать товары с нулевым количеством!')
+        if isinstance(new_product, Product):
+            self.products.append(new_product)
+            Category.total_unique += 1
+        else:
+            raise TypeError('Нельзя к продукту добавлять лишние объекты')
+
+    @property
+    def getting_list_of_product(self):
+        updated_product = ''
+        for product in self.products:
+            updated_product += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.'
+        return updated_product
+
+    @property
+    def products(self):
+        return self.products
+
+    def average(self):
+        getting_sum = 0
+        try:
+            for product in self.products:
+                getting_sum += product.price
+            result = getting_sum / len(self.products)
+            return result
+        except ZeroDivisionError:
+            return 0
+
 
 class Product:
     title: str
@@ -26,3 +65,15 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    def __str__(self):
+        return f'{self.title}, {self.price} руб. Остаток: {self.quantity}.'
+
+    def __add__(self, other):
+        if self.__class__ == type(other):
+            return self.price * self.quantity + other.__price * other.quantity
+        raise TypeError('Нельзя складывать продукты разных типов')
+
+    @classmethod
+    def creating_product(cls, product_data: dict):
+        return cls(**product_data)
